a4ea1471f64bbc00700963363cb45f7d534fb784
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2018-09-07 15:55:12 -0500

    first add

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+    // array of each character
+    var characters = {
+        "ninthDoctor": {
+            name: "The Ninth Doctor",
+            health: "120",
+            attack: "30",
+            counterAttack: "15",
+            imageUrl: "",
+        },
+        "tenthDoctor": {
+            name: "The Tenth Doctor",
+            health: "150",
+            attack: "55",
+            counterAttack: "30",
+            imageUrl: "",
+        },
+        "eleventhDoctor": {
+            name: "The Eleventh Doctor",
+            health: "135",
+            attack: "40",
+            counterAttack: "20",
+            imageUrl: "",
+        },
+        "daleks": {
+            name: "Daleks",
+            health: "140",
+            attack: "40",
+            counterAttack: "20",
+            imageUrl: "",
+        },
+        "cyberman": {
+            name: "Cyberman",
+            health: "120",
+            attack: "30",
+            counterAttack: "15",
+            imageUrl: "",
+        },
+        "theMaster": {
+            name: "The Master",
+            health: "150",
+            attack: "50",
+            counterAttack: "30",
+            imageUrl: "",
+        }
+    }
+
+// main variables
+var chosenCharacter
+var chosenEnemy
+var doctors
+var enemies
+var winningPlayer
+var battle = false;
+
+});
